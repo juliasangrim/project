10cdb8d987ef3be617f928f128a57d5466e35787
jul <y.trubitsyna@g.nsu.ru>
2021-12-09 00:08:32 +0700

    add coord class for better work with field and templates

---
+++ layout_domino/Coord.cpp
@@ -0,0 +1,32 @@
+#include "Coord.h"
+
+Coord::Coord(int x_coord, int y_coord) {
+    this -> x = x_coord;
+    this -> y = y_coord;
+}
+
+int Coord::get_x() {
+    return x;
+}
+
+int Coord::get_y() {
+    return y;
+}
+
+void Coord::set_coord(int x_coord, int y_coord) {
+    this -> x = x_coord;
+    this -> y = y_coord;
+}
+
+Coord::Coord() {
+    this -> x = 0;
+    this -> y = 0;
+
+}
+
+std::ostream &operator<<(std::ostream &out, const Coord &point) {
+    out << "( " << point.x << ";" << point.y << ")" ;
+    return out;
+}
+
+
---
+++ layout_domino/Coord.h
@@ -0,0 +1,24 @@
+
+#ifndef LAYOUT_DOMINO_COORD_H
+#define LAYOUT_DOMINO_COORD_H
+
+
+#include <ostream>
+
+struct Coord {
+private:
+    int x;
+    int y;
+public :
+    int get_x();
+    int get_y();
+    void set_coord(int x_coord, int y_coord);
+
+    Coord();
+    Coord(int x_coord, int y_coord);
+
+    friend std::ostream& operator<< (std::ostream &out, const Coord &point);
+};
+
+
+#endif //LAYOUT_DOMINO_COORD_H
